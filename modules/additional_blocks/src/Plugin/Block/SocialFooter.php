970a05d37df83a0d225501471ab0597cc242942d
Mauro <mauro@blackboxargentina.com>
2019-05-01 17:00:40 -0300

    Site updates

--- modules/additional_blocks/src/Plugin/Block/SocialFooter.php
+++ modules/additional_blocks/src/Plugin/Block/SocialFooter.php
@@ -29,7 +29,7 @@ class SocialFooter extends BlockBase {
     $footer .= '<a href="https://www.instagram.com/carbonoactivity/" class="social-link instagram-logo" target="_blank">Instagram</a>';
     $footer .= '<a href="https://twitter.com/CARBONOActivity" class="social-link twitter-logo" target="_blank">Twitter</a>';
     $footer .= '</div>';
-    $footer .= '<div class="col-md-6 col-sm-12 copyright"><span class="footer-logo">Carbono</span><br />2017 CARBONO ACTIVITY</div>';
+    $footer .= '<div class="col-md-6 col-sm-12 copyright"><span class="footer-logo">Carbono</span><br />' . date("Y") . ' CARBONO ACTIVITY</div>';
     $footer .= '<div class="col-md-6 col-sm-12 social-links social-links-right">';
     $footer .= '<a href="https://www.facebook.com/carbonoactivity/" class="social-link facebook-logo" target="_blank">Facebook</a>';
     $footer .= '<a href="https://www.instagram.com/carbonoactivity/" class="social-link instagram-logo" target="_blank">Instagram</a>';
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -17,7 +17,6 @@ class ComposerStaticInitDrupal8
         'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
-        'def43f6c87e4f8dfd0c9e1b1bab14fe8' => __DIR__ . '/..' . '/symfony/polyfill-iconv/bootstrap.php',
         'cf97c57bfe0f23854afd2f3818abb7a0' => __DIR__ . '/..' . '/zendframework/zend-diactoros/src/functions/create_uploaded_file.php',
         '9bf37a3d0dad93e29cb4e1b1bfab04e9' => __DIR__ . '/..' . '/zendframework/zend-diactoros/src/functions/marshal_headers_from_sapi.php',
         'ce70dccb4bcc2efc6e94d2ee526e6972' => __DIR__ . '/..' . '/zendframework/zend-diactoros/src/functions/marshal_method_from_sapi.php',
@@ -26,6 +25,7 @@ class ComposerStaticInitDrupal8
         '0b0974a5566a1077e4f2e111341112c1' => __DIR__ . '/..' . '/zendframework/zend-diactoros/src/functions/normalize_server.php',
         '1ca3bc274755662169f9629d5412a1da' => __DIR__ . '/..' . '/zendframework/zend-diactoros/src/functions/normalize_uploaded_files.php',
         '40360c0b9b437e69bcbb7f1349ce029e' => __DIR__ . '/..' . '/zendframework/zend-diactoros/src/functions/parse_cookie_header.php',
+        'def43f6c87e4f8dfd0c9e1b1bab14fe8' => __DIR__ . '/..' . '/symfony/polyfill-iconv/bootstrap.php',
     );
 
     public static $prefixLengthsPsr4 = array (
@@ -136,6 +136,7 @@ class ComposerStaticInitDrupal8
         ),
         'B' => 
         array (
+            'Brumann\\Polyfill\\' => 17,
             'Behat\\Mink\\Driver\\' => 18,
             'Behat\\Mink\\' => 11,
         ),
@@ -450,6 +451,10 @@ class ComposerStaticInitDrupal8
         'Composer\\Installers\\' => 
         array (
             0 => __DIR__ . '/..' . '/composer/installers/src/Composer/Installers',
+        ),
+        'Brumann\\Polyfill\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/brumann/polyfill-unserialize/src',
         ),
         'Behat\\Mink\\Driver\\' => 
         array (
